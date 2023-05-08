4ed7d0125be24f3f2e31fc12af2c610a1d64181b
Roszik Csaba <115159106+RoszikCsaba@users.noreply.github.com>
2023-05-08 22:08:20 +0200

    Update be.sh

--- my.software.hu/be.sh
+++ my.software.hu/be.sh
@@ -1,7 +1,7 @@
-export host="192.168.1.1"
-export targetFolder="/var/www/my.software.hu/service/"
-export tmpFolder="/home/teamcity/install/"
+host="192.168.1.1"
+targetFolder="/var/www/my.software.hu/service/"
+tmpFolder="/home/teamcity/install/"
 
-##teamcity[setParameter name='env.install.beHost' value='$host']
-##teamcity[setParameter name='env.install.beTargetFolder' value='$targetFolder']
-##teamcity[setParameter name='env.install.beTmpFolder' value='$tmpFolder']
+echo "##teamcity[setParameter name='env.install.beHost' value='$host']"
+echo "##teamcity[setParameter name='env.install.beTargetFolder' value='$targetFolder']"
+echo "##teamcity[setParameter name='env.install.beTmpFolder' value='$tmpFolder']"
