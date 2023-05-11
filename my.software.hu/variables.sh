42a4d65498ef6930ae287b0a021368e2ddddff9c
Roszik Csaba <115159106+RoszikCsaba@users.noreply.github.com>
2023-05-11 20:53:13 +0200

    Update variables.sh

--- my.software.hu/variables.sh
+++ my.software.hu/variables.sh
@@ -5,5 +5,6 @@ tmpFolder="/home/teamcity/install/"
 echo "##teamcity[setParameter name='env.install.beHost' value='$host']"
 echo "##teamcity[setParameter name='env.install.beTargetFolder' value='$targetFolder']"
 echo "##teamcity[setParameter name='env.install.beTmpFolder' value='$tmpFolder']"
-
+echo "--- 1 ------------"
 echo "##teamcity[setParameter name='env.content' value='%env.install.beTargetFolder% - %env.app.jwtPrivateKey%']"
+echo "--- 2 ------------"
