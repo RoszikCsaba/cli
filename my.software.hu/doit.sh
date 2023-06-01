55b9e6d46ab80e1e4bcb625c072b8e8ce89db7ca
Roszik Csaba <115159106+RoszikCsaba@users.noreply.github.com>
2023-06-01 20:35:59 +0200

    Update doit.sh

--- my.software.hu/doit.sh
+++ my.software.hu/doit.sh
@@ -1,4 +1,4 @@
 ##teamcity[setParameter name='env.install.var1' value='valueOfVar1']
 ##teamcity[setParameter name='env.install.var2' value='valueOfVar2']
-teamcity[setParameter name='env.install.var3' value='valueOfVar3']
-teamcity[setParameter name='env.install.var4' value='valueOfVar4']
+##teamcity[setParameter name='env.install.var3' value='valueOfVar3']
+##teamcity[setParameter name='env.install.var4' value='valueOfVar4']
